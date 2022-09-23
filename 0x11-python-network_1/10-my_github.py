b21296c8f65eb501dd98e1aa36f519f8ce7023fe
Fasil Solomon <98734803+samfass@users.noreply.github.com>
2022-09-23 21:33:15 +0300

    py network

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
-"""Use requests package to make a get request to given URL
-"""
+""" Fetches https://intranet.hbtn.io/status"""
 import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
 
-if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    username, token = argv[1:]
+
+    s = requests.Session()
+
+    data = {'username': username, 'token': token}
+    response = s.get(url, auth=(username, token)).json()
+    try:
+        print(response['id'])
+    except:
+        print('None')
