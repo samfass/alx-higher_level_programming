49c84fb64e3ed80a7705659acbf453b8ada521b2
Fasil Solomon <98734803+samfass@users.noreply.github.com>
2022-10-18 23:58:10 +0300

    js web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,4 +1,6 @@
-t request = require('request');
+#!/usr/bin/node
+
+const request = require('request');
 
 request(process.argv[2], function (_err, res) {
   console.log('code:', res.statusCode); // Print the response status code if a response was received
