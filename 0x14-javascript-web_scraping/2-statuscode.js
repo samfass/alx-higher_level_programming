3e39d0ea1850871b7273342f12b6fd525244bf53
samfass <sfasil374@gmail.com>
2022-10-18 20:50:45 +0000

    js web scraping

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,5 @@
+t request = require('request');
+
+request(process.argv[2], function (_err, res) {
+  console.log('code:', res.statusCode); // Print the response status code if a response was received
+});
